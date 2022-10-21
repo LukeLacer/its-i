6caaafd85a6448d8d29d70666d1056ce4755587e
Lucas Lima <lucas.lima@ciandt.com>
2022-10-21 00:23:56 -0300

    canvas draw

--- components/Canvas/index.tsx
+++ components/Canvas/index.tsx
@@ -1,16 +1,84 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from 'react';
 
-const Canvas = (props: React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>) => {
+interface ICanvasProps extends React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement> {
+    width: number;
+    height: number;
+    sendDraw: Function;
+}
+
+const Canvas = (props: ICanvasProps) => {
+    const {width, height, sendDraw} = props;
+
     const canvasStyling = {
-        width: '256px',
-        height: '256px',
+        width: `${width}px`,
+        height: `${height}px`,
         border: '1px solid black',
         cursor: 'crosshair'
+    };
+
+    const [isDrawing, setIsDrawing] = useState(false);
+    const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = width * 2;
+        canvas.height = height * 2;
+
+        const context = canvas.getContext('2d');
+        context.scale(2,2);
+        context.lineCap = 'round';
+        context.strokeStyle = 'black';
+        context.lineWidth = 5;
+        contextRef.current = context;
+    }, []);
+    
+
+    const startDrawing = ({nativeEvent}) => {
+        const {offsetX, offsetY} = nativeEvent;
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+        setIsDrawing(true);
     }
-    return <div className='canvas-wrapper'>
-        <p>Desenhe</p>
-        <canvas {...props} style={canvasStyling} />
-    </div>
+
+    const finishDrawing = () => {
+        contextRef.current.closePath();
+        setIsDrawing(false);
+    }
+
+    const draw = ({nativeEvent}) => {
+        if (!isDrawing) return
+        const {offsetX, offsetY} = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke();
+    }
+
+    const clearDraw = () => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        context.save();
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.restore();
+    }
+
+    return (<div className='canvas-wrapper'>
+        <canvas
+            {...props}
+            style={canvasStyling}
+            onMouseDown={startDrawing}
+            onMouseUp={finishDrawing}
+            onMouseOut={finishDrawing}
+            onMouseMove={draw}
+            ref={canvasRef}
+        />
+        <div className='canvas-button-wrapper'>
+            <button className='canvas-clear-button' onClick={e => clearDraw()}>Clear</button>
+            <button className='canvas-send-button' onClick={e => sendDraw(canvasRef.current.toDataURL())}>Send</button>
+        </div>
+    </div>)
 }
 
 export default Canvas;
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import styles from '../styles/Home.module.css'
 import Canvas from '../components/Canvas'
 
 export default function Home() {
+  const sendDraw = (img_data) => {
+    let img = new Image();
+    img.src = img_data;
+    alert('Essa imagem pode ser uma letra "i"');
+  }
   return (
     <>
-      <Canvas />
+      <Canvas width={256} height={256} sendDraw={sendDraw}/>
     </>
   )
 }
